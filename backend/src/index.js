42d35316383940f1d3ba82d2dee1b722f0d952a6
preethesh <preethesh4263@gmail.com>
2026-08-17 17:37:02 +0530

    change path of frontend/dist

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,11 +29,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+app.get("*", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "../../frontend/dist/index.html")
+    );
+});
 }
 
 server.listen(PORT, () => {
